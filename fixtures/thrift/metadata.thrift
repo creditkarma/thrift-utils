e99a464bdd5e2854a0997bf66c4f72512e4b03b1
Nick Nance <nick.nance@creditkarma.com>
2017-10-11 07:37:00 -0700

    working tests

---
+++ fixtures/thrift/metadata.thrift
@@ -0,0 +1,7 @@
+namespace java com.creditkarma.common
+#@namespace scala com.creditkarma.common
+
+struct Metadata {
+    1: required string appId
+    2: required string traceId
+}
---
+++ src/test/codec.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'code'
+import * as Lab from 'lab'
+
+const lab = Lab.script()
+const { describe, it, before } = lab
+export { lab }
+
+import { decoder, encoder } from '../main/codec'
+
+import { Metadata } from './generated/com/creditkarma/common/metadata'
+
+describe('codec', () => {
+    let buffer
+    describe('when encoding Thrift object', () => {
+        const metadata = new Metadata({appId: 'thrift-utils', traceId: '1234'})
+        it('resulting buffer has proper length', async () => {
+            buffer = await encoder(metadata)
+            expect(buffer.length).to.equal(31)
+        })
+    })
+
+    describe('when decoding Thrift object', () => {
+        it('resulting Thrift object should have a traceId with the proper value', async () => {
+            const metadata = await decoder(buffer, Metadata)
+            expect(metadata.traceId).to.equal('1234')
+        })
+    })
+})

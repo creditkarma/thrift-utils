692d132f41536d89b98b328521b5f649cacb5293
Kevin Greene <kevin.greene@creditkarma.com>
2017-10-17 13:27:18 -0700

    Update to use TStructLike interface from newest thrift types

--- src/test/codec.spec.ts
+++ src/test/codec.spec.ts
@@ -10,7 +10,7 @@ import { decoder, encoder } from '../main/codec'
 import { Metadata } from './generated/com/creditkarma/common/metadata'
 
 describe('codec', () => {
-    let buffer
+    let buffer: Buffer
     describe('when encoding Thrift object', () => {
         const metadata = new Metadata({appId: 'thrift-utils', traceId: '1234'})
         it('resulting buffer has proper length', async () => {
@@ -21,7 +21,7 @@ describe('codec', () => {
 
     describe('when decoding Thrift object', () => {
         it('resulting Thrift object should have a traceId with the proper value', async () => {
-            const metadata = await decoder(buffer, Metadata)
+            const metadata = await decoder(buffer.toString(), Metadata)
             expect(metadata.traceId).to.equal('1234')
         })
     })
